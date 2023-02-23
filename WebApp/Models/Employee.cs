4a50d8c57fad69fcfc7b8b114542757b63f42f62
Waqar Kabir <waqarkabir10@gmail.com>
2023-02-23 11:56:03 +0500

    Model Validation with asp-validation-for and asp-validation-summary

--- WebApp/Models/Employee.cs
+++ WebApp/Models/Employee.cs
@@ -1,11 +1,15 @@
-﻿namespace WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
 {
     public class Employee
     {
         public int Id { get; set; }
-
+        [Required]
+        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
         public string Name { get; set; }
-
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "Invalid Email")]
         public string Email { get; set; }
 
         public Dept Department { get; set; }
