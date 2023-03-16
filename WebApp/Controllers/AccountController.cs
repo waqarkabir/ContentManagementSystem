34eaedf0bb22a7e6e66893eddae8c8ad0ecb6770
Waqar Kabir <waqarkabir10@gmail.com>
2023-03-16 14:48:48 +0500

    Register New User Functionality First Part

---
+++ WebApp/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.ViewModels;
+
+namespace WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        #region Register
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult Register(RegisterViewModel VM)
+        //{ 
+            
+        //}
+        #endregion
+
+    }
+}
