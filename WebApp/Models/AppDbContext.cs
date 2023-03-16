cf2ea8030d741554c5d921dcfdbac9ae4417563b
Waqar Kabir <waqarkabir10@gmail.com>
2023-03-16 11:38:35 +0500

    Identity => Install Identity package and inherit AppDbContext from IdentityDbContext

--- WebApp/Models/AppDbContext.cs
+++ WebApp/Models/AppDbContext.cs
@@ -1,9 +1,10 @@
 ﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace WebApp.Models
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
