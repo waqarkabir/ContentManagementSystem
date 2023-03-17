38ef2d2e0d42fcf3ba192dc7890a30022caed149
Waqar Kabir <waqarkabir10@gmail.com>
2023-03-17 08:35:06 +0500

    Implemented Register and SignIn Functionality

--- WebApp/Controllers/AccountController.cs
+++ WebApp/Controllers/AccountController.cs
@@ -1,10 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using WebApp.ViewModels;
 
 namespace WebApp.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager,
+                                      SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
         #region Register
         [HttpGet]
         public IActionResult Register()
@@ -12,11 +24,32 @@ namespace WebApp.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //public IActionResult Register(RegisterViewModel VM)
-        //{ 
-            
-        //}
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel VM)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser()
+                {
+                    UserName = VM.Email,
+                    Email = VM.Email
+                };
+
+                var result = await userManager.CreateAsync(user, VM.Password);
+
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent:false);
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(VM);
+        }
         #endregion
 
     }
